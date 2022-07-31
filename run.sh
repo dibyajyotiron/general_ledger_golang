9fcbef4a334410cb8b7e3b1aac54b850e00a1ef6
dibyajyoti ghosal <dibyajyoti.ghosal@coinswitch.co>
2022-07-31 14:45:03 +0530

    variable changes + start file rename

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+source .env
+# Kill process if already running on port
+lsof -i:$APP_PORT -Fp | head -n 1 | sed 's/^p//' | xargs kill
+
+# Start using nodemon
+# export APP_ENV=local && nodemon --exec go run cmd/http/main.go --signal SIGTERM
+# Start without nodemon
+export APP_ENV=local && go run cmd/http/main.go --signal SIGTERM
+
--- start_local_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-source .env
-# Kill process if already running on port
-lsof -i:$APP_PORT -Fp | head -n 1 | sed 's/^p//' | xargs kill
-
-# Start using nodemon
-export APP_ENV=local && nodemon --exec go run main.go --signal SIGTERM
-
